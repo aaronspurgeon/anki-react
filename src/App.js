4ebe1afb4c877e97dfb11ae6fadb5cb73a6fedb7
Aaron Spurgeon <aspurgeon061@gmail.com>
2020-08-27 19:43:39 -0400

    additional setup

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
 import React from "react";
+import { Route } from "react-router-dom";
+import Home from "./comps/Home";
+import NotHome from "./comps/NotHome";
 import "./App.css";
 
 function App() {
-  return <div className="App">hello</div>;
+  return (
+    <div className="App">
+      <Route exact path="/" component={Home} />
+      <Route path="/not" component={NotHome} />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/comps/NotHome.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function NotHome() {
+    return (
+        <div>
+            <h1>not home</h1>
+        </div>
+    )
+}
